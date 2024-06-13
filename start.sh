77a65b475f95ebf3133a50ecb2579523a826d0b5
Jaks <jcqs.gr@gmail.com>
2024-06-13 23:22:20 +0200

    user table

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ composer install
 docker compose build
 docker compose up -d
 
+echo "Clearing cache..."
 docker exec -it php bin/console c:c
+
+echo "Running database migrations..."
+docker exec -it php bin/console doctrine:migrations:migrate --no-interaction
 
 echo " ✔ App running at http://localhost:8000"
