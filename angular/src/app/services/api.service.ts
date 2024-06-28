f5ee7c3d6220214ed1ffc14869393bdc76eb83eb
Jaks <jcqs.gr@gmail.com>
2024-06-28 07:38:51 +0200

    env

--- angular/src/app/services/api.service.ts
+++ angular/src/app/services/api.service.ts
@@ -2,14 +2,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = 'http://localhost:8000/api/v1'; // TODO: put in .env
-  private apiKey = 'api_key'; // TODO: put in .env
+  private apiUrl = environment.apiUrl;
+  private apiKey = environment.apiKey; 
 
   constructor(private http: HttpClient) { }
 
