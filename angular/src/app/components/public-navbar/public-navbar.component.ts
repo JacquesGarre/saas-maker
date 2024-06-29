e885245ccf2b30bb21698dc649a430a0f6f9832b
Jaks <jcqs.gr@gmail.com>
2024-06-29 15:39:20 +0200

    logged in state

--- angular/src/app/components/public-navbar/public-navbar.component.ts
+++ angular/src/app/components/public-navbar/public-navbar.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-public-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './public-navbar.component.html',
   styleUrl: './public-navbar.component.scss'
 })
 export class PublicNavbarComponent {
+
+  userIsLoggedIn: boolean = false;
+
+  constructor(authService: AuthService) {
+    this.userIsLoggedIn = authService.isAuthenticated();
+  }
 
 }
